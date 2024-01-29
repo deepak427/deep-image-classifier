f0495826fa6cc590aa0821d111990f5f4b3837e0
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-01-29 16:26:31 +0530

    loaded data

---
+++ main.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+import os
+
+# Remove dodgy images
+
+import cv2
+import imghdr
+
+data_dir = 'data'
+image_exts = ['jpeg', 'jpg', 'bmp', 'png']
+
+for image_class in os.listdir(data_dir): 
+    for image in os.listdir(os.path.join(data_dir, image_class)):
+        image_path = os.path.join(data_dir, image_class, image)
+        try: 
+            img = cv2.imread(image_path)
+            tip = imghdr.what(image_path)
+            if tip not in image_exts: 
+                print('Image not in ext list {}'.format(image_path))
+                os.remove(image_path)
+        except Exception as e: 
+            print('Issue with image {}'.format(image_path))
+            # os.remove(image_path)
+
+# Load data
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+data = tf.keras.utils.image_dataset_from_directory(data_dir)
+data_iterator = data.as_numpy_iterator()
+batch = data_iterator.next()
+
+fig, ax = plt.subplots(ncols=4, figsize=(20,20))
+for idx, img in enumerate(batch[0][:4]):
+    ax[idx].imshow(img.astype(int))
+    ax[idx].title.set_text(batch[1][idx])
+plt.show()
+
+
